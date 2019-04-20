977959886fc757135223d676b5197b197dfcf564
Renato Jiquiriçá Ramos <rjramos70@Renatos-MacBook-Pro-54283.local>
2019-04-19 23:11:43 -0300

    Atualização dos status de retorno na classe Funcionario.controller

--- app/controllers/funcionario.controller.js
+++ app/controllers/funcionario.controller.js
@@ -3,37 +3,37 @@ const Funcionario = require('../models/funcionario.model');
 exports.update = function(req, res){
     Funcionario.findByIdAndUpdate(req.params.id, {$set: req.body}, function(err, funcionario){
         if(err){
-            res.send(err);
+            res.status(500).send(err);
         }
-        res.send('Funcionário atualizado com sucesso!');
+        res.status(200).send('Funcionário atualizado com sucesso!');
     });
 }
 
 exports.delete = function(req, res){
     Funcionario.findByIdAndRemove(req.params.id, function(err){
         if(err){
-            res.send(err);
+            res.status(500).send(err);
         }
-        res.send('Funcionário removido com sucesso!');
+        res.status(200).send('Funcionário removido com sucesso!');
     });
 }
 
 exports.listAll = function(req, res){
     Funcionario.find({}, function(err, funcionarios){
         if(err){
-            res.send(err);
+            res.status(500).send(err);
         }
 
-        res.send(funcionarios);
+        res.status(200).send(funcionarios);
     });
 }
 
 exports.getById = function(req, res){
     Funcionario.findById(req.params.id, function(err, funcionatio){
         if(err){
-            res.send(err);
+            res.status(500).send(err);
         }
-        res.send(funcionatio);
+        res.status(200).send(funcionatio);
     });
 }
 
@@ -46,8 +46,8 @@ exports.create = function(req, res){
 
     funcionario.save(function(err){
         if(err){
-            res.send(err);
+            res.status(500).send(err);
         }
-        res.send('Funcionario criado com sucesso!!');
+        res.status(200).send('Funcionario criado com sucesso!!');
     })
 }
